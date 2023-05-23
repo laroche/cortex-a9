51b5350eb0856077db614b65f59903f338634f5f
Florian La Roche <Florian.LaRoche@gmail.com>
2023-05-23 18:53:02 +0200

    startup.h: adjust definition of VectorTable()

--- src/startup.h
+++ src/startup.h
@@ -2,8 +2,9 @@
 #define __STARTUP_H__
 
 #include <sys/cdefs.h>
+#include "cortex_config.h"
 
-__attribute__ ((section(".isr_vector"),naked,used)) void VectorTable(void);
+__no_stackprot __attribute__ ((section(".isr_vector"),naked,used)) void VectorTable(void);
 __attribute__ ((naked,noreturn)) void LoopHandler(void);
 int main(void);
 /* __dead2 */ void qemu_exit(void);
